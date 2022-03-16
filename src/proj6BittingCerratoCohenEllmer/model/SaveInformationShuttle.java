f86c952e6432e41b6973cd33f1d7329aba352d7f
KitchenTable99 <ccbitt23@colby.edu>
2022-03-15 21:57:48 -0400

    added file header

--- src/proj6BittingCerratoCohenEllmer/model/SaveInformationShuttle.java
+++ src/proj6BittingCerratoCohenEllmer/model/SaveInformationShuttle.java
@@ -1,3 +1,10 @@
+/*
+ * File: SaveInformationShuttle.java
+ * Names: Caleb Bitting, Matt Cerrato, Erik Cohen, Ian Ellmer
+ * Class: CS 361
+ * Project 6
+ * Date: March 18
+ */
 package proj6BittingCerratoCohenEllmer.model;
 
 import javafx.scene.control.ButtonType;
