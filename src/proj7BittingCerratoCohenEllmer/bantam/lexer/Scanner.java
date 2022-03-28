88be6c6e086539ad3f1f0c40cf6cb0ca72d72d34
KitchenTable99 <ccbitt23@colby.edu>
2022-03-28 17:01:46 -0400

    added files from moodle

---
+++ src/proj7BittingCerratoCohenEllmer/bantam/lexer/Scanner.java
@@ -0,0 +1,56 @@
+package proj7BittingCerratoCohenEllmer.bantam.lexer;
+
+import bantam.util.ErrorHandler;
+
+import java.io.Reader;
+
+/**
+ * This class reads characters from a file or a Reader
+ * and breaks it into Tokens.
+ */
+public class Scanner {
+    /**
+     * the source of the characters to be broken into tokens
+     */
+    private SourceFile sourceFile;
+    /**
+     * collector of all errors that occur
+     */
+    private ErrorHandler errorHandler;
+
+    /**
+     * creates a new scanner for the given file
+     *
+     * @param filename the name of the file to be scanned
+     * @param handler  the ErrorHandler that collects all the errors found
+     */
+    public Scanner(String filename, ErrorHandler handler) {
+        errorHandler = handler;
+        currentChar = ' ';
+        sourceFile = new SourceFile(filename);
+    }
+
+    /**
+     * creates a new scanner for the given file
+     *
+     * @param filename the name of the file to be scanned
+     * @param handler  the ErrorHandler that collects all the errors found
+     */
+    public Scanner(Reader reader, ErrorHandler handler) {
+        errorHandler = handler;
+        sourceFile = new SourceFile(reader);
+    }
+
+    /**
+     * read characters and collect them into a Token.
+     * It ignores white space unless it is inside a string or a comment.
+     * It returns an EOF Token if all characters from the sourceFile have
+     * already been read.
+     *
+     * @return the Token containing the characters read
+     */
+    public Token scan() {
+        return null;  // REMOVE THIS LINE AND REPLACE IT WITH YOUR CODE
+    }
+
+}
