726157bded6c406440e1432471defb491816ca2d
mattcerrato <mattcerrato01@gmail.com>
2022-05-05 10:32:47 -0400

    added additional double checking

--- src/proj10BittingCerratoCohenEllmer/bantam/lexer/precusortokens/PrecursorNumberToken.java
+++ src/proj10BittingCerratoCohenEllmer/bantam/lexer/precusortokens/PrecursorNumberToken.java
@@ -1,14 +1,17 @@
 /*
- * File: PrecursorIntegerToken.java
- * Author: cbitting
- * Date: 4/8/2021
+ * File: PrecursorNuumberToken.java
+ * Author: cbitting, matt cerrato
+ * Date: 5/5/2022
  */
 package proj10BittingCerratoCohenEllmer.bantam.lexer.precusortokens;
 
 import proj10BittingCerratoCohenEllmer.bantam.lexer.Token;
 import proj10BittingCerratoCohenEllmer.bantam.util.Error;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
+enum type {Double, Int}
 
 /**
  * If some token starts with a digit, this PrecursorToken contains all the logic needed
@@ -17,6 +20,9 @@ import java.util.Stack;
 public class PrecursorNumberToken extends AbstractPrecursorToken {
 
     private boolean onePoint;
+    private type tokenType = type.Int;
+    private List<Character> letterChars = List.of('d', 'e', 'E', 'f');
+    private List<Character >mathChars = List.of('+', '-', '.');
 
     public PrecursorNumberToken(Stack<Character> sc, int n, String s) {
         super(sc, n, s);
@@ -28,13 +34,17 @@ public class PrecursorNumberToken extends AbstractPrecursorToken {
         spellingStack.push(c);
 
         if (!Character.isDigit(c)) {
-             if(!this.onePoint && c == '.'){
-                this.onePoint = true;
+            if(letterChars.contains(c)||mathChars.contains(c)){
+                pushDoubleChar(c);
             }else{
-                 containsCompleteToken = true;
-                 popLastBeforeCreation = true;
-             }
+                containsCompleteToken = true;
+                popLastBeforeCreation = true;
+            }
         }
+    }
+
+    public void pushDoubleChar(char c){
+        tokenType = type.Double;
     }
 
     @Override
@@ -44,22 +54,23 @@ public class PrecursorNumberToken extends AbstractPrecursorToken {
             throw new MalformedSpellingStackException("You need to pop the stack first");
         }
 
+
+        Token.Kind tokenKind = Token.Kind.ERROR;
+
         // decide if number is integer or double
-        Token.Kind tokenKind;
         try{
             String stack = makeStackString(true);
-            if(stack.contains(".")){
+            if(tokenType.equals(type.Double)){
                 Double.parseDouble(stack);
                 tokenKind = Token.Kind.DBLCONST;
-            }else {
+            }else if(tokenType.equals(type.Int)) {
                 Integer.parseInt(stack);
                 tokenKind = Token.Kind.INTCONST;
             }
         }catch(NumberFormatException e){
             tokenError.add(new Error(Error.Kind.LEX_ERROR, filename,
                     currentLineNumber,
-                    "Number Constant too large!"));
-            tokenKind = Token.Kind.ERROR;
+                    "Number Constant not valid"));
         }
 
         return new Token(tokenKind, makeStackString(false), currentLineNumber);
